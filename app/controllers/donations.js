f0f883ca0df1d3e405f92f0dde47a79650745613
Eamonn de Leastar <edeleastar@wit.ie>
2019-01-21 09:50:46 +0000

    include routes + actions

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -5,6 +5,16 @@ const Donations = {
     handler: async function(request, h) {
       return h.file('./app/views/main.html');
     }
+  },
+  signup: {
+    handler: async function(request, h) {
+      return h.file('./app/views/signup.html');
+    }
+  },
+  login: {
+    handler: async function(request, h) {
+      return h.file('./app/views/login.html');
+    }
   }
 };
 
