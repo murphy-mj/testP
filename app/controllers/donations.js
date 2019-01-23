0c088505b0a5574d402bc9edf3281b6548e075f9
Eamonn de Leastar <edeleastar@wit.ie>
2019-01-23 08:14:13 +0000

    remove async

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -2,17 +2,17 @@
 
 const Donations = {
   index: {
-    handler: async function(request, h) {
+    handler: function(request, h) {
       return h.file('./app/views/main.html');
     }
   },
   signup: {
-    handler: async function(request, h) {
+    handler: function(request, h) {
       return h.file('./app/views/signup.html');
     }
   },
   login: {
-    handler: async function(request, h) {
+    handler: function(request, h) {
       return h.file('./app/views/login.html');
     }
   }
