2c519f915147739d73164538b395b8b1cc167b8e
Eamonn de Leastar <edeleastar@wit.ie>
2019-01-25 08:56:40 +0000

    include and configure hapi-auth-cookie

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ server.bind({
 async function init() {
   await server.register(require('inert'));
   await server.register(require('vision'));
+  await server.register(require('hapi-auth-cookie'));
 
   server.views({
     engines: {
@@ -27,6 +28,13 @@ async function init() {
     partialsPath: './app/views/partials',
     layout: true,
     isCached: false
+  });
+
+  server.auth.strategy('standard', 'cookie', {
+    password: 'secretpasswordnotrevealedtoanyone',
+    cookie: 'donation-cookie',
+    isSecure: false,
+    ttl: 24 * 60 * 60 * 1000,
   });
 
   server.route(require('./routes'));
