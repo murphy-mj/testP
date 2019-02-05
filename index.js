5f556158737dcc7acc16df3731846de973c290bb
Eamonn de Leastar <edeleastar@wit.ie>
2019-02-05 17:22:31 +0000

    make server mind to environment port (if available)

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ if (result.error) {
 const Hapi = require('hapi');
 
 const server = Hapi.server({
-  port: 3000,
-  host: 'localhost'
+  port: process.env.PORT || 3000,
 });
 
 require('./app/models/db');
