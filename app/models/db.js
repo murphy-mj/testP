65f54c4a35027d3d86f089fa5bd60cda4b4bcef0
Eamonn de Leastar <edeleastar@wit.ie>
2019-02-08 10:33:52 +0000

    load simple user + donation model

--- app/models/db.js
+++ app/models/db.js
@@ -7,6 +7,15 @@ const Mongoose = require('mongoose');
 Mongoose.connect(process.env.db);
 const db = Mongoose.connection;
 
+async function seed() {
+  var seeder = require('mais-mongoose-seeder')(Mongoose);
+  const data = require('./initdata.json');
+  const Donation = require('./donation');
+  const User = require('./user');
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
+
 db.on('error', function(err) {
   console.log(`database connection error: ${err}`);
 });
@@ -17,4 +26,5 @@ db.on('disconnected', function() {
 
 db.once('open', function() {
   console.log(`database connected to ${this.name} on ${this.host}`);
+  seed();
 })
