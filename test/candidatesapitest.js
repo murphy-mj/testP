8ea690b56a9ce2a494a2b1247e5061630f94cfbc
Eamonn de Leastar <edeleastar@wit.ie>
2019-02-24 09:47:23 +0000

    first simple candidates test

---
+++ test/candidatesapitest.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('chai').assert;
+const axios = require('axios');
+
+suite('Candidate API tests', function () {
+
+  test('get candidates', async function () {
+    const response = await axios.get('http://localhost:3000/api/candidates');
+    console.log(response.data);
+  });
+});
