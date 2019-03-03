d3bf22571296e8efbded383d6fdd487f4dfae247
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-03 13:25:37 +0000

    introduce client service class for donation

---
+++ test/donation-service.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const axios = require('axios');
+const baseUrl = 'http://localhost:3000';
+
+class DonationService {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getCandidates() {
+    const response = await axios.get(this.baseUrl + '/api/candidates');
+    return response.data;
+  }
+
+  async getCandidate(id) {
+    try {
+      const response = await axios.get(this.baseUrl + '/api/candidates/' + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async createCandidate(newCandidate) {
+    try {
+      const response = await axios.post(this.baseUrl + '/api/candidates', newCandidate);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async deleteAllCandidates() {
+    try {
+      const response = await axios.delete(this.baseUrl + '/api/candidates');
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async deleteOneCandidate(id) {
+    try {
+      const response = await axios.delete(this.baseUrl + '/api/candidates/' + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+}
+
+module.exports = DonationService;
