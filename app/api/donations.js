6425d1455005c8b0dc743227197c7cd3c67c3012
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-05 12:57:25 +0000

    get donations by candidate

--- app/api/donations.js
+++ app/api/donations.js
@@ -4,10 +4,17 @@ const Boom = require('boom');
 const Donation = require('../models/donation');
 
 const Donations = {
-  find: {
+  findAll: {
     auth: false,
     handler: async function(request, h) {
       const donations = await Donation.find();
+      return donations;
+    }
+  },
+  findByCandidate: {
+    auth: false,
+    handler: async function(request, h) {
+      const donations = await Donation.find({ candidate: request.params.id });
       return donations;
     }
   }
