28d94ca9fce345fd7b85666eb868fbcc6b467e57
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-18 11:57:49 +0000

    defactor candidates to work against secured routes

--- test/candidatesapitest.js
+++ test/candidatesapitest.js
@@ -5,42 +5,52 @@ const DonationService = require('./donation-service');
 const fixtures = require('./fixtures.json');
 const _ = require('lodash');
 
-suite('Candidate API tests', function () {
-
+suite('Candidate API tests', function() {
   let candidates = fixtures.candidates;
   let newCandidate = fixtures.newCandidate;
+  let newUser = fixtures.newUser;
 
   const donationService = new DonationService(fixtures.donationService);
 
-  setup(async function () {
+  suiteSetup(async function() {
+    await donationService.deleteAllUsers();
+    const returnedUser = await donationService.createUser(newUser);
+    const response = await donationService.authenticate(newUser);
+  });
+
+  suiteTeardown(async function() {
+    await donationService.deleteAllUsers();
+    donationService.clearAuth();
+  });
+
+  setup(async function() {
     await donationService.deleteAllCandidates();
   });
 
-  teardown(async function () {
+  teardown(async function() {
     await donationService.deleteAllCandidates();
   });
 
-  test('create a candidate', async function () {
+  test('create a candidate', async function() {
     const returnedCandidate = await donationService.createCandidate(newCandidate);
     assert(_.some([returnedCandidate], newCandidate), 'returnedCandidate must be a superset of newCandidate');
     assert.isDefined(returnedCandidate._id);
   });
 
-  test('get candidate', async function () {
+  test('get candidate', async function() {
     const c1 = await donationService.createCandidate(newCandidate);
     const c2 = await donationService.getCandidate(c1._id);
     assert.deepEqual(c1, c2);
   });
 
-  test('get invalid candidate', async function () {
+  test('get invalid candidate', async function() {
     const c1 = await donationService.getCandidate('1234');
     assert.isNull(c1);
     const c2 = await donationService.getCandidate('012345678901234567890123');
     assert.isNull(c2);
   });
 
-
-  test('delete a candidate', async function () {
+  test('delete a candidate', async function() {
     let c = await donationService.createCandidate(newCandidate);
     assert(c._id != null);
     await donationService.deleteOneCandidate(c._id);
@@ -48,7 +58,7 @@ suite('Candidate API tests', function () {
     assert(c == null);
   });
 
-  test('get all candidates', async function () {
+  test('get all candidates', async function() {
     for (let c of candidates) {
       await donationService.createCandidate(c);
     }
@@ -57,7 +67,7 @@ suite('Candidate API tests', function () {
     assert.equal(allCandidates.length, candidates.length);
   });
 
-  test('get candidates detail', async function () {
+  test('get candidates detail', async function() {
     for (let c of candidates) {
       await donationService.createCandidate(c);
     }
@@ -68,9 +78,8 @@ suite('Candidate API tests', function () {
     }
   });
 
-  test('get all candidates empty', async function () {
+  test('get all candidates empty', async function() {
     const allCandidates = await donationService.getCandidates();
     assert.equal(allCandidates.length, 0);
   });
-
 });
